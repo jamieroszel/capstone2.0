6519252ad1da3c6bb4d31bdced5a21208613a741
jamieroszel <jamie.s.roszel@gmail.com>
2021-07-11 23:29:44 -0400

    update about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,9 +7,8 @@ const About = () => {
         <Layout>
             <div>
                 <h1>About Me</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis a facilis atque saepe non ipsam quae, temporibus, eius quisquam ipsa qui neque nemo similique doloribus accusamus, quia odit error itaque.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis a facilis atque saepe non ipsam quae, temporibus, eius quisquam ipsa qui neque nemo similique doloribus accusamus, quia odit error itaque.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis a facilis atque saepe non ipsam quae, temporibus, eius quisquam ipsa qui neque nemo similique doloribus accusamus, quia odit error itaque.</p>
+                <p>Hi there, after 16 years spent teaching high school English, I recently made the jump to technical writing.</p>
+              
             </div>
         </Layout>
     );
